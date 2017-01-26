d221b41a02222d1838da62f944ad9731e3882480
Samuel <samuelchen173@gmail.com>
2017-01-25 21:05:17 -0500

    Initial commit

---
+++ android/src/com/worldengine/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.worldengine.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.worldengine.game.WorldEngine;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new WorldEngine(), config);
+	}
+}
